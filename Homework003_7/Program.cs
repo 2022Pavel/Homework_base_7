6c02b92f55c891a2cba845a29f540d839c58c77d
Pavel <ierpvk@gmail.com>
2022-08-12 15:45:58 +0500

    Homework003_7 project

---
+++ Homework003_7/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел.
+ Найдите среднее арифметическое элементов в каждом столбце.*/
+
+ Random random = new Random();
+int[,] arr = new int[random.Next(1, 5), random.Next(1, 5)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.WriteLine();
+    Console.Write($"Среднее арифметическое столбца { sum / arr.GetLength(0)} ");
+}
